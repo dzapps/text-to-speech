17388be2eb5f40d10ac13a10eeb52ba03f169377
ashiqsetu <setu037@gmail.com>
2019-09-28 18:13:57 +0600

    First Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var textToSpeechConverter = document.getElementById("textToSpeechConverter");
+var textField = document.getElementById("textField");
+var speak = document.getElementsByClassName("speak");
+var speech = document.getElementsByClassName("speech");
+
+textToSpeechConverter.addEventListener('submit', talk, false);
+
+function talk(e) {
+	e.preventDefault();
+	//var text = $('input[name="text"]').val();
+	var text = textField.value;
+	text = encodeURIComponent(text);
+	var url = "http://translate.google.com/translate_tts?tl=en&q=" + text + "&client=tw-ob";
+	$('audio').attr('src',url).get(0).play();
+	//$('.speech').attr('src',url).get(0).play();		
+}
+
+
+
+
+
+
+
